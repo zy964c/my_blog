b86368bedca4834f407311ea131d9ca2f8cb73a8
zy964c <zy964c@gmail.com>
2017-06-29 08:55:33 +0000

    sing in button removed

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from datetime import datetime
-from app import app, db, lm, oid
+from app import app, db, lm
 from .forms import LoginForm, EditForm, PostForm, SearchForm
 from .models import User, Post
 from config import POSTS_PER_PAGE, MAX_SEARCH_RESULTS
